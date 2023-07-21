5fa8364aad878c9ee08bcfe604aac111abcb7835
ndk1219 <ndk1219@gmail.com>
2023-07-21 21:34:33 +0900

    アプリ更新２回目

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,28 +10,27 @@ public class App {
 		Scanner scan = new Scanner(System.in);
 		int row = scan.nextInt();
 		int column = scan.nextInt();
-		String[][] sit = new String[row][column];		//席を準備
+		String[][] sit = new String[row][column];								//席を準備
 		int count = 0;
-		String[] nameList=new String[row*column];
+		String[] nameList=new String[row*column];								//名前を入れる要素を準備
 		
 		for (int i=0;i<nameList.length;i++) {									//名前を入力
 			System.out.println("名前を入力してください。");
 			System.out.println("入力が終わったら[終了]と入力してください。");
 			String name = scan.next();
+			nameList[i]=name;
 			System.out.printf("座る人数は%sです。", nameList.length);
 			
 		}
-		
-		
-		
-		System.out.println("記入された名前を席にランダム配置します。");
+		System.out.println("名前をランダムな要素数に代入します。");			//名前を席の要素に配置
 		Random r = new Random();
-		for(int i=0;i<100;i++) {
-			int num = r.nextInt()+1;
+		for(int i=0;i<100;i++) {												//100回名前の要素順を入れ替える　以下入れ替え処理
+			int num = r.nextInt(nameList.length-1)+1;//ランダムな値を代入
+			String temp=nameList[0];//作ったランダムな値をtempに入れる
+			nameList[0]=nameList[num];//
+			nameList[num]=temp;//
 		}
-		
-		
-		
+		System.out.println(Arrays.toString(nameList));	
 		
 		for(int i=0;i<count;i++) {
 			for(int j=0;j<sit.length;j++) {
